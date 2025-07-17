ea600db59564cc00af1e1f7de2e8fde4d09f4f65
LevelUpMarketers <admin@leveluprichmond.com>
2025-07-17 12:52:24 -0500

    committing twilio things

---
+++ app/public/wp-content/plugins/tta-management-plugin/twilio-config.php
@@ -0,0 +1,14 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! defined( 'TTA_TWILIO_SID' ) ) {
+    define( 'TTA_TWILIO_SID', 'SK4163b03da378ba62d974275c3c6a4297' );
+}
+if ( ! defined( 'TTA_TWILIO_TOKEN' ) ) {
+    define( 'TTA_TWILIO_TOKEN', 'XLrv5ZYrfF7e7yU8uZziFyV9ANsbdFxP' );
+}
+if ( ! defined( 'TTA_TWILIO_FROM' ) ) {
+    define( 'TTA_TWILIO_FROM', '+18049448422' );
+}
