f36b9100ee8be73a17c355b510108d3e989193a4
LevelUpMarketers <admin@leveluprichmond.com>
2025-08-08 09:44:25 -0400

    feat: make event page ad sticky

--- app/public/wp-content/plugins/tta-management-plugin/assets/js/frontend/sticky-events-ad.js
+++ app/public/wp-content/plugins/tta-management-plugin/assets/js/frontend/sticky-events-ad.js
@@ -2,9 +2,16 @@
   function init(){
     var isWide = window.matchMedia('(min-width: 769px)').matches;
     var $sidebars = $('.tta-events-right, .tta-event-right');
+
+    // Calculate offsets once per run rather than inside each sidebar loop.
+    var headerHeight = $('.site-header, .tta-header').first().outerHeight() || 0;
+    var adminBarHeight = $('body').hasClass('admin-bar') ? $('#wpadminbar').outerHeight() || 0 : 0;
+    var extraOffset = 148;
+
     $sidebars.each(function(){
       var $el = $(this);
       var inst = $el.data('stickyInstance');
+
       if(!isWide){
         if(inst){
           inst.destroy();
@@ -12,10 +19,8 @@
         }
         return;
       }
+
       if(!inst){
-        var headerHeight = $('.site-header, .tta-header').first().outerHeight() || 0;
-        var adminBarHeight = $('body').hasClass('admin-bar') ? $('#wpadminbar').outerHeight() || 0 : 0;
-        var extraOffset = 148;
         inst = new StickySidebar(this, {
           innerWrapperSelector: '.tta-events-ad',
           topSpacing: headerHeight + adminBarHeight + extraOffset,
